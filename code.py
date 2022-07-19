962b51265f254e6bcc55547e66564673089e5292
Reiley Batelaan <56047532+ReileyB@users.noreply.github.com>
2022-07-19 15:23:00 -0700

    Update code.py

--- code.py
+++ code.py
@@ -1,5 +1,8 @@
 # SPDX-FileCopyrightText: Brent Rubell for Adafruit Industries
 # SPDX-License-Identifier: MIT
+import supervisor
+supervisor.disable_autoreload()
+
 
 import time
 from microcontroller import cpu
@@ -11,6 +14,13 @@ from adafruit_esp32spi import adafruit_esp32spi_wifimanager
 import adafruit_esp32spi.adafruit_esp32spi_socket as socket
 import adafruit_minimqtt.adafruit_minimqtt as MQTT
 from adafruit_io.adafruit_io import IO_MQTT
+
+from buzzersongs import BuzzerSongs
+import digitalio
+from lcd1602 import LCD
+import adafruit_rgbled
+from rainbowio import colorwheel
+import adafruit_thermistor
 
 ### WiFi ###
 
@@ -36,6 +46,20 @@ wifi = adafruit_esp32spi_wifimanager.ESPSPI_WiFiManager(esp, secrets)
 led_pin = DigitalInOut(board.LED)
 led_pin.switch_to_output()
 
+# Configure other peripherals
+lcd = LCD()
+
+song = BuzzerSongs(board.GP16)
+
+red = board.GP7
+green = board.GP8
+blue = board.GP9
+led = adafruit_rgbled.RGBLED(red, blue, green)
+led.color = (222, 49, 99)
+
+thermo = adafruit_thermistor.Thermistor(board.GP26, 10000.0, 10000.0, 25.0, 3950.0, high_side=False)
+
+
 # Define callback functions which will be called when certain events happen.
 # pylint: disable=unused-argument
 def connected(client):
@@ -55,7 +79,7 @@ def disconnected(client):
 
 
 def on_led_msg(client, topic, message):
-    # Method called whenever user/feeds/led has a new value
+    # Method called whenever user/feeds/reiley/led has a new value
     print("New message on topic {0}: {1} ".format(topic, message))
     if message == "ON":
         led_pin.value = True
@@ -63,6 +87,24 @@ def on_led_msg(client, topic, message):
         led_pin.value = False
     else:
         print("Unexpected message on LED feed.")
+
+def on_song_msg(client, topic, message):
+    # Methoc called whenever user/feeds/reiley/song has a new value
+    print("New message on topic {0}: {1} ".format(topic, message))
+    song.play(message)
+
+def on_lcd_msg(client, topic, message):
+    # Methoc called whenever user/feeds/reiley/lcd has a new value
+    print("New message on topic {0}: {1} ".format(topic, message))
+    lcd.clear()
+    message = str(message)
+    lcd.message(message.replace('\\n','\n'))
+
+def on_color_msg(client, topic, message):
+    # Methoc called whenever user/feeds/reiley/color has a new value
+    print("New message on topic {0}: {1} ".format(topic, message))
+    led.color = eval(message)
+    
 
 
 # Connect to WiFi
@@ -75,7 +117,7 @@ MQTT.set_socket(socket, esp)
 
 # Initialize a new MQTT Client object
 mqtt_client = MQTT.MQTT(
-    broker=secrets["broker"],
+    broker=secrets["mqtt_broker"],
     port=8883,
     username=secrets["mqtt_username"],
     password=secrets["mqtt_password"],
@@ -89,15 +131,21 @@ io.on_connect = connected
 io.on_disconnect = disconnected
 io.on_subscribe = subscribe
 
-# Set up a callback for the led feed
-io.add_feed_callback("klaus/led", on_led_msg)
+# Set up a callbacks
+io.add_feed_callback("reiley/led", on_led_msg)
+io.add_feed_callback("reiley/song", on_song_msg)
+io.add_feed_callback("reiley/lcd", on_lcd_msg)
+io.add_feed_callback("reiley/color", on_color_msg)
 
 # Connect to Adafruit IO
 print("Connecting to MQTT Broker...")
 io.connect()
 
-# Subscribe to all messages on the led feed
-io.subscribe("klaus/led")
+# Subscribe to all messages on the feeds
+io.subscribe("reiley/led")
+io.subscribe("reiley/song")
+io.subscribe("reiley/lcd")
+io.subscribe("reiley/color")
 
 prv_refresh_time = 0.0
 while True:
@@ -114,11 +162,17 @@ while True:
     if (time.monotonic() - prv_refresh_time) > 30:
         # take the cpu's temperature
         cpu_temp = cpu.temperature
+        thermo_temp = thermo.temperature
         # truncate to two decimal points
         cpu_temp = str(cpu_temp)[:5]
+        thermo_temp = str(thermo_temp)[:5]
         print("CPU temperature is %s degrees C" % cpu_temp)
+        print("Thermistor temperature is %s degrees C" % thermo_temp)
         # publish it to io
         print("Publishing %s to temperature feed..." % cpu_temp)
-        io.publish("klaus/temperature", cpu_temp)
+        print("Publishing %s to thermo temp feed..." % thermo_temp)
+        io.publish("reiley/temperature", cpu_temp)
+        io.publish("reiley/thermotemp", thermo_temp)
         print("Published!")
         prv_refresh_time = time.monotonic()
+    time.sleep(0.01)
